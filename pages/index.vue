bff7f9e79f98e673c17a9acf52826ebe1d7e80d9
kevin olson <acidjazz@gmail.com>
2021-04-03 03:37:19 -0500

    🎨 look and feel touchups

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div class="flex items-center justify-center w-screen h-screen bg-indigo-50">
     <div class="w-1/4 text-xs flex flex-col justify-center items-center">
       <icon-fume class="w-1/2 h-1/2" primary="text-indigo-200" secondary="text-indigo-800" />
-      <div class="flex flex-col items-left space-y-2">
-        <div class="flex items-center space-x-2 mx-8">
+      <div class="flex flex-col bg-white border rounded-md divide-gray-200 divide-y ">
+        <div class="flex items-center space-x-2 px-3 py-2">
           <icon-spinner v-if="!ssr" class="w-6 h-6" />
           <icon-check
             v-else
@@ -14,9 +14,13 @@
           <span>Server-Side rendering</span>
           <span class="text-gray-500">({{ version }})</span>
         </div>
-        <div class="flex items-center space-x-2 mx-8">
+        <div class="flex items-center space-x-2 px-3 py-2">
           <img src="/favicon.ico" class="w-6 h-6" alt="favicon">
           <span>Statically sourced image</span>
+        </div>
+        <div class="flex items-center space-x-2 px-3 py-2">
+          <icon-aws class="w-6 h-6" />
+          <span>Docker test build number <span class="font-semibold">1</span></span>
         </div>
       </div>
     </div>
