6cb15b028e8cd21b231506174a66dd09482cd169
kevin olson <acidjazz@gmail.com>
2021-04-03 03:43:49 -0500

    :geen_heart: update round 2

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="flex items-center space-x-2 px-3 py-2">
           <icon-aws class="w-6 h-6" />
-          <span>Docker test build number <span class="font-semibold">1</span></span>
+          <span>Docker test build number <span class="font-semibold">2</span></span>
         </div>
       </div>
     </div>
