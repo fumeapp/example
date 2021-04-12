0d45d688267a8e1de640fba33f7a57b4431103d4
kevin olson <acidjazz@gmail.com>
2021-04-12 01:55:11 -0500

    🚧 add a console-log - build 45

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="flex items-center space-x-2 px-3 py-2">
           <icon-aws class="w-6 h-6" />
-          <span>Docker test build number <span class="font-semibold">44</span></span>
+          <span>Docker test build number <span class="font-semibold">45</span></span>
         </div>
         <div class="flex items-center space-x-2 px-3 py-2">
           <icon-cog class="w-4 h-4" />
@@ -66,6 +66,7 @@ export default Vue.extend({
       this.$nuxt.$loading.start()
       setTimeout(() => this.$nuxt.$loading.finish(), 500)
     })
+    console.log('fume example console.log() message')
   }
 })
 </script>
